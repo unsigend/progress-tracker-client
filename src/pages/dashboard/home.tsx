f66814478e84b88367abcdd941a974197c31b8cc
qiuyixiang <qiuyixiang2003@gmail.com>
2025-09-29 10:39:53 -0500

    add frontend component upload file for backend endpoint

--- src/pages/dashboard/home.tsx
+++ src/pages/dashboard/home.tsx
@@ -1,5 +1,19 @@
+// import components
+import FileUploadButton from "@/components/modules/ui/fileUpload";
+import { toast } from "sonner";
+/**
+ * Dashboard Home Page
+ */
 const DashboardHomePage = () => {
-    return null;
+    const handleFileUploadSuccess = (fileUrl: string) => {
+        console.log(fileUrl);
+        toast.success("File uploaded successfully!");
+    };
+    return (
+        <div>
+            <FileUploadButton onUploadSuccess={handleFileUploadSuccess} />
+        </div>
+    );
 };
 
 export default DashboardHomePage;
